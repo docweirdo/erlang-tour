6f9d5328c9687ac043bda57fe00947983de0e086
Corin Baurmann <corin.baurmann@fu-berlin.de>
2020-08-04 13:07:35 +0200

    supervisor: changed map to tuple

--- catshop_sup.erl
+++ catshop_sup.erl
@@ -9,8 +9,8 @@ open_shop() ->
 init([]) ->
     {ok, {{one_for_all, 1, 60},
         [
-            #{id=>one, start=>{catshop_clerk_2_3, start_shift, [pink]}},
-            #{id=>two, start=>{catshop_clerk_2_3, start_shift, [black]}},
-            #{id=>three, start=>{catshop_clerk_2_3, start_shift, [brown]}}
+            {one, {catshop_clerk_2_3, start_shift, [pink]}, permanent, 5000, worker, [start_shift]},
+            {two, {catshop_clerk_2_3, start_shift, [black]}, permanent, 5000, worker, [start_shift]},
+            {three, {catshop_clerk_2_3, start_shift, [brown]}, permanent, 5000, worker, [start_shift]}
         
         ]}}.
